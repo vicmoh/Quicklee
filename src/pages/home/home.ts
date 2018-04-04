60f065144ae2608faf4fa1800c1f9f1597da83a4
vicmoh <vimoh1213@gmail.com>
2018-04-04 15:17:56 -0400

    asd

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -13,19 +13,19 @@ export class HomePage {
   //constructor
   constructor(public navCtrl: NavController) {
     this.items = [];
-      //date
-      var tempDate = "April 1, 2018";
-      var item;
-      //add items
-      item = this.addItem("Justing Bieber", tempDate, "Studying at McLaughlin Library", 1, 1, "bieber");
-      console.log(item);
-      this.items.push(item);
-      item = this.addItem("Parthy Boy", tempDate, "Partayy at Brass tap boyzz", 5, 5, "why");
-      console.log(item);
-      this.items.push(item);
-      item = this.addItem("Vic Developer", tempDate, "Pi lab is my home.", 1, 1, "vic");
-      console.log(item);
-      this.items.push(item);
+    //date
+    var tempDate = "April 1, 2018";
+    var item;
+    //add items
+    item = this.addItem("Justing Bieber", tempDate, "Studying at McLaughlin Library", 1, 1, "bieber");
+    console.log(item);
+    this.items.push(item);
+    item = this.addItem("Parthy Boy", tempDate, "Partayy at Brass tap boyzz", 5, 5, "why");
+    console.log(item);
+    this.items.push(item);
+    item = this.addItem("Vic Developer", tempDate, "Pi lab is my home.", 1, 1, "vic");
+    console.log(item);
+    this.items.push(item);
   }//end constructor
 
   addItem(newName: string, newDate: string, newStatus: string, newDistance: number, newTime: number, newImage: string){
--- src/pages/messages/messages.ts
+++ src/pages/messages/messages.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ChatPage } from '../chat/chat';
 
 /**
  * Generated class for the MessagesPage page.
@@ -14,12 +15,59 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'messages.html',
 })
 export class MessagesPage {
+  //dec intance
+  onlineItems: any[];
+  recentItems: any[];
+  
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    //must dec
+    this.onlineItems = [];
+    this.recentItems = [];
+    // adding the onlineItems for the recent
+    var onlineItem = this.additem("avatar-finn", "Finn", "I'm a big deal", "Listen, I've had a pretty messed up day");
+    this.onlineItems.push(onlineItem);
+    console.log(onlineItem);
+    onlineItem = this.additem("avatar-han", "Han", "Look, kid", "I've got enough on my plate as it is, and I");
+    this.onlineItems.push(onlineItem);
+    console.log(onlineItem);
+    onlineItem = this.additem("avatar-rey", "Rey", "I can handle myself", "You will remove these restraints and leave");
+    this.onlineItems.push(onlineItem);
+    console.log(onlineItem);
+    onlineItem = this.additem("avatar-luke", "Luke", "Your thoughts betray you", "I feel the good in you, the conflict");
+    this.onlineItems.push(onlineItem);
+    console.log(onlineItem);
+    // adding people who are online
+    var recentItem = this.additem("avatar-poe", "Poe", "New Ride", "I just upgraded my X-Wing. Next time");
+    this.recentItems.push(recentItem);
+    console.log(recentItem);
+    recentItem = this.additem("avatar-ben", "Ben", "Move Along", "These aren't the droids you're looking for");
+    this.recentItems.push(recentItem);
+    console.log(recentItem);
+    recentItem = this.additem("avatar-leia", "Leia", "You're My Only Hope", "I've placed information vital to the survival");
+    this.recentItems.push(recentItem);
+    console.log(recentItem);
+    recentItem = this.additem("avatar-yoda", "Yoda", "Size matters not", "Do or do not. There is no try");
+    this.recentItems.push(recentItem);
+    console.log(recentItem);
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad MessagesPage');
   }
 
-}
+  additem(newImage:string, newName: string, newStatus: string, newMessage: string){
+    var item = {
+      image: newImage,
+      name: newName,
+      status: newStatus,
+      message: newMessage 
+    };
+    return item;
+  }//end func
+
+  goToChat(){
+    this.navCtrl.push(ChatPage);
+  }//end func
+
+}//end class
