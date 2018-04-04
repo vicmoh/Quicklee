1d17c5e6742b0236263b71347ed8e9e95eff6214
vicmoh <vimoh1213@gmail.com>
2018-04-04 08:28:37 -0400

    added contact and avatar home

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -17,24 +17,25 @@ export class HomePage {
       var tempDate = "April 1, 2018";
       var item;
       //add items
-      item = this.addItem("Justing Bieber", tempDate, "Studying at McKinnon", 1, 1);
+      item = this.addItem("Justing Bieber", tempDate, "Studying at McLaughlin Library", 1, 1, "bieber");
       console.log(item);
       this.items.push(item);
-      item = this.addItem("Parthy Boy", tempDate, "Partayy at Brass tap boyzz", 5, 5);
+      item = this.addItem("Parthy Boy", tempDate, "Partayy at Brass tap boyzz", 5, 5, "why");
       console.log(item);
       this.items.push(item);
-      item = this.addItem("Vic Creator", tempDate, "Pi lab is my home", 1, 1);
+      item = this.addItem("Vic Developer", tempDate, "Pi lab is my home.", 1, 1, "vic");
       console.log(item);
       this.items.push(item);
   }//end constructor
 
-  addItem(newName: string, newDate: string, newStatus: string, newDistance: number, newTime: number){
+  addItem(newName: string, newDate: string, newStatus: string, newDistance: number, newTime: number, newImage: string){
     var item = {
       name: newName,
       date: newDate,
       status: newStatus,
       distance: newDistance, 
-      time: newTime
+      time: newTime,
+      image: newImage
     };
     return item;
   }//end func
