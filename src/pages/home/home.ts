5adb108833532e0b8fec69ff8a9055a3979cd2e6
vicmoh <vimoh1213@gmail.com>
2018-04-04 05:39:37 -0400

    added chat bubbles

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -26,7 +26,7 @@ export class HomePage {
       item = this.addItem("Vic Creator", tempDate, "Pi lab is my home", 1, 1);
       console.log(item);
       this.items.push(item);
-  }
+  }//end constructor
 
   addItem(newName: string, newDate: string, newStatus: string, newDistance: number, newTime: number){
     var item = {
@@ -37,37 +37,5 @@ export class HomePage {
       time: newTime
     };
     return item;
-  }
-
-  //add button
-  cardButton(setIcon: string, setText: string): string{
-    var guiString = "<ion-col>";
-    + "<button ion-button icon-left clear small>";
-    + "<ion-icon name=\""+setIcon+"\"></ion-icon>";
-    + "<div>"+setIcon+"</div>";
-    + "</button>";
-    + "</ion-col>";
-    return guiString;
-  }
-
-  //add new cards
-  addCard(newName: string, newDate: string, newDistance: string, newTime: string): string{
-    // set header
-    var name = "<h2>"+ newName + "</h2>";
-    var date = "<p>"+newDate+"</p>";
-    var item = "<ion-item>"+ name + date +"</ion-item>";
-    var time =  "<ion-col center text-center>"
-              + "<ion-note>"
-              + newTime
-              + "</ion-note>"
-              + "</ion-col>"
-  
-    // set the card
-    var b1 = this.cardButton("pin", newDistance);
-    var b2 = this.cardButton ("text", "Messeges");
-    var row = "<ion-row>" + b1 + b2 + "</ion-row>";
-    var card = "<ion-card>"+item + row + time + "</ion-card>";
-    //this.listOfCards.push(card)
-    return card;
-  }
+  }//end func
 }
