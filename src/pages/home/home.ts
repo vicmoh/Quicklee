26423e3389e5c0e6808faa882560a51a2df2ee1e
vicmoh <vimoh1213@gmail.com>
2018-04-03 23:47:14 -0400

    added dynamic cards

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,11 +7,37 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
   //vars
-  listOfCards;
-  showFriends;
+  items: any[];
+  item: any;
 
   //constructor
-  constructor(public navCtrl: NavController) {}
+  constructor(public navCtrl: NavController) {
+    this.items = [];
+      //date
+      var tempDate = "April 1, 2018";
+      var item;
+      //add items
+      item = this.addItem("Justing Bieber", tempDate, "Studying at McKinnon", 1, 1);
+      console.log(item);
+      this.items.push(item);
+      item = this.addItem("Parthy Boy", tempDate, "Partayy at Brass tap boyzz", 5, 5);
+      console.log(item);
+      this.items.push(item);
+      item = this.addItem("Vic Creator", tempDate, "Pi lab is my home", 1, 1);
+      console.log(item);
+      this.items.push(item);
+  }
+
+  addItem(newName: string, newDate: string, newStatus: string, newDistance: number, newTime: number){
+    var item = {
+      name: newName,
+      date: newDate,
+      status: newStatus,
+      distance: newDistance, 
+      time: newTime
+    };
+    return item;
+  }
 
   //add button
   cardButton(setIcon: string, setText: string): string{
@@ -44,11 +70,4 @@ export class HomePage {
     //this.listOfCards.push(card)
     return card;
   }
-
-  getCardList(){
-    this.listOfCards.push(this.addCard("Mark", "5/1/18", "Within 100m", "1h ago"));
-    this.listOfCards.push(this.addCard("Parth", "5/1/18", "Within 2km", "2h ago"));
-    this.listOfCards.push(this.addCard("Vick", "5/1/18", "Within 5km", "5h ago"));
-  }
-  
 }
