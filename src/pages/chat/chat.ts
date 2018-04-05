533cddc41de035a20c8c90af245ef47d63a41a58
vicmoh <vimoh1213@gmail.com>
2018-04-05 06:18:14 -0400

    add dynamic chat

--- src/pages/chat/chat.ts
+++ src/pages/chat/chat.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 /**
@@ -12,26 +12,42 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 @Component({
   selector: 'page-chat',
   templateUrl: 'chat.html',
+  queries: {
+    content: new ViewChild('content')
+  }
 })
 export class ChatPage {
-
+  @ViewChild('scroll') scroll: any;
   listOfTexts: any[];
   message: string;
   text: any;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.listOfTexts = [];
   }
 
   ionViewDidLoad() {
+    setTimeout(() => {
+      this.scroll.scrollToBottom(300);
+    }, 1000);
     console.log('ionViewDidLoad ChatPage');
   }
 
   sendMessage(){
     //get the ngModal input message and send 
+    if(this.message == ""){
+      console.log("message is empty");
+      return;
+    }//end if
     this.text = this.message;
     this.message = "";
     console.log("text message = " + this.text);
     this.listOfTexts.push(this.text);
+    console.log("end of sendMessage()");
+    // this.scroll.scrollToBottom();
+    setTimeout(() => {
+      this.scroll.scrollToBottom(300);
+    }, 1000);
   }//end func
 
 }
