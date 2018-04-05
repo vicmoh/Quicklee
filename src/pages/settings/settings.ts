0d4f933f63293420fb7b7ebd0c6a953968fd87bb
vicmoh <vimoh1213@gmail.com>
2018-04-04 22:02:52 -0400

    asd

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { LoginPage } from '../login/login';
 
 /**
  * Generated class for the SettingsPage page.
@@ -20,6 +21,10 @@ export class SettingsPage {
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SettingsPage');
+  }
+
+  logout(){
+    this.navCtrl.push(LoginPage);
   }
 
 }
